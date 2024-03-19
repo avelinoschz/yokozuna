3065cd6cb71a8ffa7f5a8f0b043cf6a3903b6037
Avelino Sanchez <avelino.schz@gmail.com>
2024-03-18 22:30:17 -0600

    add neetcode 6_1 comment

---
+++ neetcode/ads-beginners/2_linked_lists/6_1_design_double_linked_list.py
@@ -0,0 +1,4 @@
+# This exercise was done in the leetcode problem 707. Design Linked List
+# https://leetcode.com/problems/design-linked-list/description/
+# cd ../../leetcode/707_design_linked_list.py
+# cd ../../leetcode/neetcode-solutions/707_design_linked_list_neet.py
