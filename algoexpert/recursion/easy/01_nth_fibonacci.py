a2fdc41936b159a26ac01cf94d4b1f69a4609bea
Avelino Sanchez <avelino.schz@gmail.com>
2024-03-21 13:51:36 -0600

    algoexpert: fibonacci desc

--- algoexpert/recursion/easy/01_nth_fibonacci.py
+++ algoexpert/recursion/easy/01_nth_fibonacci.py
@@ -1,12 +1,14 @@
+# https://www.algoexpert.io/questions/nth-fibonacci
+
 # The Fibonacci sequence is defined as follows: the first number of the 
-# sequence is o, the second number is 1, and the nth number is the sum 
+# sequence is `0`, the second number is `1`, and the nth number is the sum 
 # of the (n - 1)th and (n - 2)th numbers.
 
-# Write a function that takes in an integer and returns the nth Fibonacci number.
+# Write a function that takes in an integer `n` and returns the nth Fibonacci number.
 
 # Important note: the Fibonacci sequence is often defined with its first two numbers as
-# FO = 0 and F1 = 1. For the purpose of this question, the first Fibonacci number is
-# FO; therefore, getNthFib (1) is equalto FO, getNthFib(2) is equalto F1, etc..
+# `FO = 0` and `F1 = 1`. For the purpose of this question, the first Fibonacci number is
+# `FO`; therefore,` getNthFib(1)` is equalto `FO`, `getNthFib(2)` is equalto `F1`, etc..
 
 # Sample Input #1
 # n = 2
