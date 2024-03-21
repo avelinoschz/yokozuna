68615290f15f06d60076513c9d05018d33ce1dcd
Avelino Sanchez <avelino.schz@gmail.com>
2024-03-21 13:49:17 -0600

    algoexpert: add reverse linked list desc

---
+++ algoexpert/linked_lists/hard/02_reverse_linked_list.py
@@ -0,0 +1,36 @@
+# https://www.algoexpert.io/questions/reverse-linked-list
+
+# Write a function that takes in the head of a Singly Linked List, reverses 
+# the list in place (i.e., doesn't create a brand new list), and returns its 
+# new head.
+
+# Each `LinkedList` node has an integer `value` as well as a `next` node in 
+# the list or to `None` / `null` if it's the tail of the list.
+
+# You can assume that the input Linked List will always have at least one node; 
+# in other words, the head will never be `None` / `null`.
+
+# Sample Input
+# head = 0 > 1 -> 2 → 3 →> 4 →> 5 // the head node with value 0
+
+# Sample Output
+# 5 → 4 → 3 → 2 → 1 → 0 // the new head node with value 5
+
+# O(n) time | O(1) space - where `n` is the number of nodes in the Linked List
+
+# This is an input class. Do not edit.
+class LinkedList:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+def reverseLinkedList(head):
+    prev = None
+    curr = head
+    while curr:
+        temp_next = curr.next
+        curr.next = prev
+        prev = curr
+        curr = temp_next
+    return prev
