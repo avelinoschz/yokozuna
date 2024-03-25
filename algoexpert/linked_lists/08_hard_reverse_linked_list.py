a3c1e4bd1e6a4bda15fa5ce920eafd65a271145b
Avelino Sanchez <avelino.schz@gmail.com>
2024-03-24 21:27:46 -0600

    algoexpert(linked lists): refactor

--- algoexpert/linked_lists/08_hard_reverse_linked_list.py
+++ algoexpert/linked_lists/08_hard_reverse_linked_list.py
@@ -19,6 +19,11 @@
 # Optimal:
 # O(n) time | O(1) space - where `n` is the number of nodes in the Linked List
 
+# Didn't add any AlgoExpert solution, since the solution using iteration
+# was basically the same. Recursion wasn't covered.
+
+# -----------------------------------------------
+
 # This is an input class. Do not edit.
 class LinkedList:
     def __init__(self, value):
