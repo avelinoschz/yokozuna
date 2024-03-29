6f2edfefffa58a0ef41f492abf637db4bcc3e0f2
Avelino Sanchez <avelino.schz@gmail.com>
2024-03-28 23:07:57 -0600

    neetcode(ads-beginners): sol design double linked list

--- neetcode/ads-beginners/02_linked_lists/06_3_sol_design_doubly_linked_list.py
+++ neetcode/ads-beginners/02_linked_lists/06_3_sol_design_doubly_linked_list.py
@@ -1,4 +1,6 @@
+# This excerise does not have a proper solution,  since it takes advantage is a 
+# leetcode question
+
 # This exercise was done in the leetcode problem 707. Design Linked List
-# The course uses takes advantage that there is already a leetcode problem
 # https://leetcode.com/problems/design-linked-list/description/
 # cd ../../leetcode/neetcode-solutions/707_design_linked_list_neet.py
